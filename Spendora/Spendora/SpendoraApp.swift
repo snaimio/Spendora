d01f73bd019e3992304bf1c883a435c589595624
Sheikh Naim <naimbgd@gmail.com>
2026-05-29 12:12:51 -0400

    Created my Xcode project Spendora Samrt Subscription and Spending Tracker

---
+++ Spendora/Spendora/SpendoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendoraApp.swift
+//  Spendora
+//
+//  Created by Sheikh Naim on 2026-05-29.
+//
+
+import SwiftUI
+
+@main
+struct SpendoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
