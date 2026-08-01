f9047bf79146c59c2ac2d84c2863d89e9f097dbe
Sheikh Naim <naimbgd@gmail.com>
2026-07-31 23:32:06 -0400

    fix: pass sortOption binding to SearchBarView in SubscriptionListView

--- Spendora/Spendora/Views/Subscriptions/SubscriptionListView.swift
+++ Spendora/Spendora/Views/Subscriptions/SubscriptionListView.swift
@@ -54,7 +54,7 @@ struct SubscriptionListView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
-                SearchBarView(searchText: $searchText)
+                SearchBarView(searchText: $searchText, sortOption: $sortOption)
                     .padding(.horizontal, 12)
                     .padding(.vertical, 8)
                 
